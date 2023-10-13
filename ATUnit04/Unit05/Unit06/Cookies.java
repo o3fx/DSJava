bdb3ca26e9799590aac819fce3752d129db7b954
o3fx <billzhang5261@gmail.com>
2023-10-12 22:09:20 -0400

    Last Part of HW 06
    Last Part of HW 06

---
+++ ATUnit04/Unit05/Unit06/Cookies.java
@@ -0,0 +1,24 @@
+package Unit05.Unit06;
+import java.util.Scanner;
+public class Cookies {
+    public static void main (String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many cookies do you want to buy: ");
+        double cookies = input.nextDouble();
+        final double COST = .99;
+
+        if (cookies >= 24){
+            double totalCost = (cookies * COST) * .85;
+            System.out.printf("Total cost: $%.2f%n", totalCost);
+        } else if (cookies >= 12){
+            double totalCost = (cookies * COST) * .95;
+            System.out.printf("Total cost: $%.2f%n", totalCost);
+        } else if (cookies >= 6){
+            double totalCost = (cookies * COST) * .975;
+            System.out.printf("Total cost: $%.2f%n", totalCost);
+        }
+    
+
+    }
+}
+
